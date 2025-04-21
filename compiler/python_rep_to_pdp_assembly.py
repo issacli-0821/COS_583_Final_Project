b644bdc1fd4ebe6019b8172ec7a14cd8e64d890f
issacli-0821 <tl1719@princeton.edu>
2025-04-21 12:13:10 -0400

    Implemented translate_load

--- compiler/python_rep_to_pdp_assembly.py
+++ compiler/python_rep_to_pdp_assembly.py
@@ -1,24 +1,44 @@
 import llvmlite
-import re
-
-
+from enum import Enum
 
 # Dictionary from identifier to location on the stack
 class Environment:
     def __init__(self):
         self.env : dict[str, int] = {}
-        self.nextOffset = 0
+        self.next_offset = 0
     
-    def get(self, identifier: str) -> int:
-        return self.env[identifier]
+    def get(self, identifier: str) -> str:
+        return str(self.env[identifier]) + "(SP)"
     
     def add(self, identifier: str, size: int):
-        self.env[identifier] = self.nextOffset
-        self.nextOffset += size
+        print(f"Added {identifier} to {self.next_offset}")
+        self.env[identifier] = self.next_offset
+        self.next_offset += size
+
+    # TODO?
+    def update(self, identifier: str):
+        return
     
     def remove(self, identifier: str) -> int:
         return self.env.pop(identifier)
 
+
+# Enum to represent opcodes
+class Opcode(Enum):
+    MOV = "mov"
+    ADD = "add"
+    RET = "ret"
+
+
+class Instruction:
+    def __init__(self, opcode: Opcode, operand1: str, operand2: str):
+        self.opcode = opcode
+        self.operand1 = operand1
+        self.operand2 = operand2
+    
+    def __str__(self):
+        return f"{self.opcode.name} {self.operand1}, {self.operand2}"
+    
 
 """
 define dso_local i32 @add(i32 noundef %0, i32 noundef %1) #0 {
@@ -45,7 +65,7 @@ def print_module(module: llvmlite.binding.module.ModuleRef):
     return
 
 
-def python_rep_to_pdp_assembly(module: llvmlite.binding.module.ModuleRef) -> list[str]:
+def python_rep_to_pdp_assembly(module: llvmlite.binding.module.ModuleRef) -> list[Instruction]:
     all_instructions = []
     # print_module(module)
     for function in module.functions:
@@ -55,7 +75,7 @@ def python_rep_to_pdp_assembly(module: llvmlite.binding.module.ModuleRef) -> lis
     return all_instructions
     
 
-def translate_function(function: llvmlite.binding.value.ValueRef) -> list[str]:
+def translate_function(function: llvmlite.binding.value.ValueRef) -> list[Instruction]:
     env = Environment()
     # Populate env with function inputs?
 
@@ -67,7 +87,7 @@ def translate_function(function: llvmlite.binding.value.ValueRef) -> list[str]:
     return all_instructions
 
 
-def translate_block(block: llvmlite.binding.value.ValueRef, env: Environment) -> list[str]:
+def translate_block(block: llvmlite.binding.value.ValueRef, env: Environment) -> list[Instruction]:
     all_instructions = []
 
     for instr in block.instructions:
@@ -76,7 +96,8 @@ def translate_block(block: llvmlite.binding.value.ValueRef, env: Environment) ->
 
     return all_instructions
 
-def translate_instruction(instr: llvmlite.binding.value.ValueRef, env: Environment) -> list[str]:
+
+def translate_instruction(instr: llvmlite.binding.value.ValueRef, env: Environment) -> list[Instruction]:
 
     print(instr)
     print("type: ", instr.type)
@@ -103,58 +124,62 @@ def translate_instruction(instr: llvmlite.binding.value.ValueRef, env: Environme
         
         case _:
             raise NotImplementedError(f"Instruction {instr.opcode} not supported yet.")
-  
 
-def translate_alloca(instr, env: Environment) -> list[str]:
 
-    # from pprint import pprint
-    # print(dir(instr))
-    print("Name here---%s---" % (instr.name))
-    # env.add()
+def get_identifier_from_instruction(instr) -> str:
 
-    # print(instr.name)
+    str_instr = str(instr).lstrip()
+    identifier = str_instr[:str_instr.index(" ")]
 
-    return []  
+    return identifier
+
+
+
+def translate_alloca(instr, env: Environment) -> list[Instruction]:
+
+    env.add(get_identifier_from_instruction(instr), 2)
+    return []
 
 
 """
 instr format: add [flags] <type> <op1>, <op2>
 """
-def translate_add(instr, env: Environment) -> list[str]:
+def translate_add(instr, env: Environment) -> list[Instruction]:
     for op in instr.operands:
        add_op = str(op.name)
-       
-
-    
-    
-    
-
-    
 
     
     return []
 
 
-def translate_store(instr, env: Environment) -> list[str]:
+def translate_store(instr, env: Environment) -> list[Instruction]:
     return []
 
 
 
 """
-  %5 = load i32, i32* %3, align 4
+%5 = load i32, i32* %3, align 4
 type:  i32
 opcode:  load
 Operand:    %3 = alloca i32, align 4
 """
-def translate_load(instr, env: Environment) -> list[str]:
-    operands = instr.operands
-    for op in operands:
-        print("Operand in load: ", op.name)
-        print(type(op.name))
+def translate_load(instr, env: Environment) -> list[Instruction]:
+    operand = list(instr.operands)[0]
+    # print("Operand from load: ", operand)
+
+    identifier = get_identifier_from_instruction(instr)
+    # print("Identifier from load: ", identifier)
+
+    operand_identifier = get_identifier_from_instruction(operand)
+    # print(operand_identifier)
+    
+    env.add(identifier, 2)
+    instruction = Instruction(Opcode.MOV, env.get(operand_identifier), env.get(identifier))
+    print(instruction)
     
         
-    return []
+    return [instruction]
 
 
-def translate_ret(instr, env: Environment) -> list[str]:
+def translate_ret(instr, env: Environment) -> list[Instruction]:
     return []
