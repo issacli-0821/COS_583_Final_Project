29af06881d761bafa3fe9e6371bbdcaef67f5217
issacli-0821 <tl1719@princeton.edu>
2025-04-28 22:05:33 -0400

    Updated twoConditons.c

--- example_c_files/twoConditions.c
+++ example_c_files/twoConditions.c
@@ -4,11 +4,11 @@ int main() {
     int numTwo = 2;
     int finalResult = 0;
 
-    if (numOne > 10) {
+    if (numOne <= 10) {
         finalResult = finalResult + 5;
     }
 
-    if (numTwo < 5) {
+    if (numTwo <= 5) {
         finalResult = finalResult + 3;
     }
 
