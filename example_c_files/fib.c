8e979c746ffa37d8aa33772f4c7b7f4d10caea0c
issacli-0821 <tl1719@princeton.edu>
2025-04-28 21:10:25 -0400

    Computed the 10th fib number

--- example_c_files/fib.c
+++ example_c_files/fib.c
@@ -3,12 +3,12 @@
 #include <stdio.h>
 
 int main() {
-    int n = 30;
+    int n = 10;
     int first = 0;
     int second = 1;
     int fib = 0;
 
-    for (int i = 2; i <= n; i++) {
+    for (int i = 1; i <= n; i++) {
         fib = first + second;
         first = second;
         second = fib;
